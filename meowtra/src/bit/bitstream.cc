509183c457a011ac8e3d4cee929c552ba5cccfc3
gatecat <gatecat@ds0.me>
2022-01-31 19:47:53 +0000

    Bitstream file reading
    Signed-off-by: gatecat <gatecat@ds0.me>

--- meowtra/src/bit/bitstream.cc
+++ meowtra/src/bit/bitstream.cc
@@ -1,5 +1,46 @@
 #include "bitstream.h"
 
+#include <array>
+
 MEOW_NAMESPACE_BEGIN
+
+namespace {
+    bool scan_preamble(std::istream &stream, uint32_t preamble) {
+        index_t count = 0;
+        uint32_t shiftreg = 0;
+        while (!stream.eof()) {
+            if (count >= 4 && shiftreg == preamble) {
+                return true;
+            }
+            uint8_t next = 0;
+            stream.read(reinterpret_cast<char*>(&next), 1);
+            shiftreg = (shiftreg << 8) | uint32_t(next);
+        }
+        return false;
+    }
+}
+
+RawBitstream RawBitstream::read(std::istream &in) {
+    RawBitstream result;
+    // TODO: metadata before preamble
+    // Scan for preamble
+    bool found = scan_preamble(in, 0xAA995566);
+    MEOW_ASSERT(found);
+
+    std::array<uint8_t, 4> buf;
+    while (!in.eof()) {
+        in.read(reinterpret_cast<char*>(buf.data()), buf.size());
+        result.words.base.push_back(
+            (uint32_t(buf[0]) << 24U) |
+            (uint32_t(buf[1]) << 16U) |
+            (uint32_t(buf[2]) <<  8U) |
+            (uint32_t(buf[3])       )
+        );
+        if (in.gcount() != 4)
+            break;
+    }
+
+    return result;
+}
 
 MEOW_NAMESPACE_END
--- meowtra/src/bit/bitstream.h
+++ meowtra/src/bit/bitstream.h
@@ -17,6 +17,18 @@ struct RawBitstream {
     static RawBitstream read(std::istream &in);
 };
 
+struct BitstreamPacket {
+    enum : uint8_t {
+        NOP = 0x00,
+        READ = 0x01,
+        WRITE = 0x02,
+    } op;
+
+    uint16_t reg;
+
+    Chunk<uint32_t> payload;
+};
+
 MEOW_NAMESPACE_END
 
 #endif
