a3cc6ac1178257c67152b60127cb8dfead57ff5e
gatecat <gatecat@ds0.me>
2022-02-19 18:59:53 +0000

    Give the Python package a sensible name
    Signed-off-by: gatecat <gatecat@ds0.me>

--- settings/xczu7ev.sh
+++ settings/xczu7ev.sh
@@ -2,6 +2,7 @@ BASE_PATH=$(dirname $(readlink -f "${BASH_SOURCE:-$0}"))
 export MEOW_PART="xczu7ev-ffvf1517-2-e"
 export MEOW_WORK="${BASE_PATH}/../work/zu7ev"
 export MEOW_DEVDB="${BASE_PATH}/../database/xcup/zu7ev"
+export PYTHONPATH="${BASE_PATH}/.."
 mkdir -p $MEOW_WORK
 mkdir -p $MEOW_DEVDB
 
